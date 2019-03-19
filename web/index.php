02a7a4f96fcb3eeb70252a1f34a2608bc2ca1294
andrew623849 <K122918510@gmail.com>
2019-03-20 01:49:52 +0800

    不要用tcpdf

--- web/index.php
+++ web/index.php
@@ -4,8 +4,7 @@ date_default_timezone_set("Asia/Taipei");
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-// require_once('../tools/tcpdf/examples/lang/chi.php');
-require_once('../tools/tcpdf/tcpdf.php');
+
 require __DIR__ . '/../tools/model.php';
 require __DIR__ . '/../tools/function_show.php';
 require __DIR__ . '/../vendor/autoload.php';
