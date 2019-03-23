d05a212fc3d1ea280af0f70418c5a126f4a9dc57
andrew623849 <K122918510@gmail.com>
2019-03-23 23:47:24 +0800

    toothcase get 換成post

--- views/site/toothcase.php
+++ views/site/toothcase.php
@@ -7,21 +7,19 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\toothcaseSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
-$request = Yii::$app->request;
-$clinic_id=$request->get('toothcaseSearch');
-$clinic=ArrayHelper::map($clinic_info,'id','clinic');
-$clinic_id['clinic_id']=$clinic_id['clinic_id']==NULl?1:$clinic_id['clinic_id'];
-$this->title = $clinic[$clinic_id['clinic_id']].'病例';
+
+$clinic = $clinic_info[$clinic_id-1]['clinic'];
+$this->title = $clinic.'病例';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<?php  echo $this->render('_search', ['clinic_info' => $clinic_info,'model' => $searchModel,'clinic'=>$clinic_id['clinic_id']]); ?>
+<?php  echo $this->render('_search', ['clinic_info' => $clinic_info,'model' => $searchModel,'clinic'=>$clinic_id]); ?>
 <div style="margin-left:1px"  > 
-<?php  echo $this->render('pdf_month', ['clinic_info' => $clinic_info,'model' => $searchModel,'clinic'=>$clinic_id['clinic_id']]); ?>
+<?php  echo $this->render('pdf_month', ['clinic_info' => $clinic_info,'model' => $searchModel,'clinic'=>$clinic_id]); ?>
 </div>
 <div class="toothcase-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?= Html::a('新增病例',['create','clinic_this' => $clinic_id['clinic_id']], ['class' => 'btn btn-success']) ?>
+    <?= Html::a('新增病例',['create','clinic_this' => $clinic_id], ['class' => 'btn btn-success']) ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
