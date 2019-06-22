b4f808837cbce98b50dc26b519384f303d5dd773
tester9527 <03131045@gm.scu.edu.tw>
2019-06-22 11:12:34 +0800

    交件 材料顯示錯誤

--- views/site/todaycase.php
+++ views/site/todaycase.php
@@ -26,12 +26,12 @@ for($i = 0;$i < count($model);$i ++){
         $options = "";
     }
     if($clinic != $models['clinic_id']){
-        echo '<h2>'.$clinic_info[($models['clinic_id']-1)]['clinic'].'</h2>';
+        echo '<hr><div class="col-sm-12"><h2>'.$clinic_info[($models['clinic_id']-1)]['clinic'].'</h2></div>';
         $clinic=$models['clinic_id'];
     }
 
     ?>
-    <div class="col-sm-4">
+    <div class="col-sm-4 <?php if($models['material_id_1'] != 0) echo 'hidden' ?>">
     <?php echo DetailView::widget([
         'model' => $models,
         'attributes' => [
@@ -39,7 +39,44 @@ for($i = 0;$i < count($model);$i ++){
             ['label'=>'收件日','value'=>$models->start_time],
             ['label'=>'交件日','value'=>$models->end_time],
             ['label'=>'病人姓名','value'=>$models->name],
-            ['label'=>'材料1','value'=>$material_info[($models->material_id)-1]['material']],
+            ['label'=>'材料1','value'=>$material_info[($models->material_id)]['material']],
+            ['label'=>'齒位','value'=>$models->tooth],
+            ['label'=>'齒色','value'=>$models->tooth_color],
+            ['label'=>'備註','value'=>$models->remark],
+            ['label'=>'金額','value'=>$models->price],
+        ],
+        'options'=>['class' => 'table table-striped table-bordered detail-view','style'=>$options],
+    ]) ?>
+    </div>
+        <div class="col-sm-4 <?php if($models['material_id_1'] == 0) echo 'hidden' ?>" >
+    <?php echo DetailView::widget([
+        'model' => $models,
+        'attributes' => [
+            ['label'=>'診所','value'=>$clinic_info[($models->clinic_id-1)]['clinic']],
+            ['label'=>'收件日','value'=>$models->start_time],
+            ['label'=>'交件日','value'=>$models->end_time],
+            ['label'=>'病人姓名','value'=>$models->name],
+            ['label'=>'材料1','value'=>$material_info[($models->material_id)]['material']],
+            ['label'=>'齒位','value'=>$models->tooth],
+            ['label'=>'齒色','value'=>$models->tooth_color],
+            ['label'=>'材料2','value'=>$material_info[($models->material_id_1)==0?0:($models->material_id_1)-1]['material']],
+            ['label'=>'齒位','value'=>$models->tooth_1],
+            ['label'=>'齒色','value'=>$models->tooth_color_1],
+            ['label'=>'備註','value'=>$models->remark],
+            ['label'=>'金額','value'=>$models->price],
+        ],
+        'options'=>['class' => 'table table-striped table-bordered detail-view','style'=>$options],
+    ]) ?>
+    </div>
+        <div class="col-sm-4 <?php if($models['material_id_2'] == 0) echo 'hidden' ?>" >
+    <?php echo DetailView::widget([
+        'model' => $models,
+        'attributes' => [
+            ['label'=>'診所','value'=>$clinic_info[($models->clinic_id-1)]['clinic']],
+            ['label'=>'收件日','value'=>$models->start_time],
+            ['label'=>'交件日','value'=>$models->end_time],
+            ['label'=>'病人姓名','value'=>$models->name],
+            ['label'=>'材料1','value'=>$material_info[($models->material_id)]['material']],
             ['label'=>'齒位','value'=>$models->tooth],
             ['label'=>'齒色','value'=>$models->tooth_color],
             ['label'=>'材料2','value'=>$material_info[($models->material_id_1)==0?0:($models->material_id_1)-1]['material']],
