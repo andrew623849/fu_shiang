9aa422236253a36201e944a510a87a52c02e5c8c
andrew623849 <K122918510@gmail.com>
2019-12-22 22:52:03 +0800

    新增病例的方式用svg圖

--- views/toothcase/_form.php
+++ views/toothcase/_form.php
@@ -5,6 +5,8 @@ use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use yii\web\JsExpression;
 use dosamigos\datepicker\DatePicker;
+use kartik\slider\Slider;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\toothcase */
@@ -51,43 +53,54 @@ $material_data = \app\models\MaterialSearch::ShowData('all',['useable'=>0,'delet
     <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'name')->label("*病人姓名")->textInput(['maxlength' => true]) ?></div>
     <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model,'clinic_id')->label("*診所")->dropDownList(ArrayHelper::map($clinic_info,'id','clinic'),['style'=>'border:1px solid ;'])?></div>
     <div class="form-group col-sm-12" style="background-color:#E8E8E8;">
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model,'material_id')->label("*材料1")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;'])?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth')->label("*齒位")->textInput(['maxlength' => true]) ?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth_color')->label("齒色")->textInput(['maxlength' => true]) ?></div>
-        <?php if($url == 'create'){ ?>
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price')->label("其他費用")->textInput(['value' => 0]) ?></div>
-        <?php }else{ ?> 
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price')->label("其他費用")->textInput() ?></div>
-        <?php } ?>
-    </div>
+		<div class="form-group col-sm-4">
+			<?= $form->field($model, 'tooth')->label("*齒位1")->hiddenInput(['maxlength' => true]) ?>
+			<?php include("tooth.svg"); ?>
+		</div>
+		<div class="form-group col-sm-8"><?= $form->field($model,'material_id')->label("*材料1")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;'])?>
+			<?= $form->field($model, 'tooth_color')->label("齒色")->textInput(['maxlength' => true]) ?>
+			<?php if($url == 'create'){ ?>
+				<?= $form->field($model, 'other_price')->label("其他費用")->textInput(['value' => 0]) ?>
+			<?php }else{ ?>
+				<?= $form->field($model, 'other_price')->label("其他費用")->textInput() ?>
+			<?php } ?>
+		</div>
+	</div>
     <div class="form-group col-sm-12 material_id_1" style="background-color:#E8E8E8;" <?php if($model['material_id_1'] == 0){echo 'hidden';$val= "'value'=>0";}else{$val='';}?>>
         <input class="btn btn-danger" style="margin-left: 98.3%;" type="button" id="m_del_btn_1" value="x">
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model,'material_id_1')->label("*材料2")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;',$val])?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth_1')->label("*齒位")->textInput(['maxlength' => true]) ?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth_color_1')->label("齒色")->textInput(['maxlength' => true]) ?></div>
-        <?php if($url == 'create'){ ?>
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price_1')->label("其他費用")->textInput(['value' => 0]) ?></div>
-        <?php }else{ ?> 
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price_1')->label("其他費用")->textInput() ?></div>
-        <?php } ?>
+		<div class="form-group col-sm-4">
+			<?= $form->field($model, 'tooth_1')->label("*齒位2")->hiddenInput(['maxlength' => true]) ?>
+			<?php include("tooth1.svg"); ?>
+		</div>
+		<div class="form-group col-sm-8"><?= $form->field($model,'material_id_1')->label("*材料2")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;'])?>
+			<?= $form->field($model, 'tooth_color_1')->label("齒色")->textInput(['maxlength' => true]) ?>
+			<?php if($url == 'create'){ ?>
+					<?= $form->field($model, 'other_price_1')->label("其他費用")->textInput(['value' => 0]) ?>
+				<?php }else{ ?>
+					<?= $form->field($model, 'other_price_1')->label("其他費用")->textInput() ?>
+				<?php } ?>
+		</div>
     </div>
     <div class="form-group col-sm-12 material_id_2" style="background-color:#E8E8E8;" <?php if($model['material_id_2'] == 0){echo 'hidden';$val= "'value'=>0";}else{$val='';}?>>
         <input class="btn btn-danger right" style="margin-left:  98.3%;" type="button" id="m_del_btn_2" value="x">
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model,'material_id_2')->label("*材料3")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;',$val])?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth_2')->label("*齒位")->textInput(['maxlength' => true]) ?></div>
-        <div class="form-group col-sm-6" style="height:99px;"><?= $form->field($model, 'tooth_color_2')->label("齒色")->textInput(['maxlength' => true]) ?></div>
-        <?php if($url == 'create'){ ?>
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price_2')->label("其他費用")->textInput(['value' => 0]) ?></div>
-        <?php }else{ ?> 
-            <div class="form-group col-sm-6"> <?= $form->field($model, 'other_price_2')->label("其他費用")->textInput() ?></div>
-        <?php } ?>
+		<div class="form-group col-sm-4">
+			<?= $form->field($model, 'tooth_2')->label("*齒位3")->hiddenInput(['maxlength' => true]) ?>
+			<?php include("tooth2.svg"); ?>
+		</div>
+		<div class="form-group col-sm-8"><?= $form->field($model,'material_id_2')->label("*材料3")->dropDownList(ArrayHelper::map($material_data,'id','material'),['style'=>'border:1px solid ;'])?>
+			<?= $form->field($model, 'tooth_color_2')->label("齒色")->textInput(['maxlength' => true]) ?>
+			<?php if($url == 'create'){ ?>
+					<?= $form->field($model, 'other_price_2')->label("其他費用")->textInput(['value' => 0]) ?>
+				<?php }else{ ?>
+					<?= $form->field($model, 'other_price_2')->label("其他費用")->textInput() ?>
+				<?php } ?>
+		</div>
     </div>
     <input class="col-sm-12 btn btn-primary" type="button" id="m_add_btn" value="新增材料+">
     <div class="form-group col-sm-12"> <?= $form->field($model, 'remark')->label("備註")->textarea(['rows' => '3']) ?></div>
     <div class="form-group col-sm-12"> <?= $form->field($model, 'price')->label("")->hiddenInput(['value' => -1]) ?></div>
-        
 
-    <div class="form-group col-sm-4"><?= Html::submitButton('提交', ['class' => 'btn btn-success']) ?></div>
+	<div class="form-group col-sm-4"><?= Html::submitButton('提交', ['class' => 'btn btn-success']) ?></div>
     
     <?php ActiveForm::end(); ?>
 
@@ -122,5 +135,6 @@ $js =<<< JS
         $('#toothcase-tooth_color_2').val('');
         v = 1;
     });
+
 JS;
 $this->registerJs($js);
