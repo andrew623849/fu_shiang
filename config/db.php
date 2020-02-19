d3bf1d53fa873d315953f641a4b9340e1cef2b14
andrew623849 <K122918510@gmail.com>
2020-02-19 21:14:28 +0800

    資料庫調整

--- config/db.php
+++ config/db.php
@@ -1,36 +1,30 @@
 <?php
-
 	if(!empty($_SERVER['HTTP_HOST'])){
 		$dbname = explode('.',$_SERVER['HTTP_HOST'])[0];
 		if($dbname != 'cowbtool' && $dbname != 'cowbtest'){
 			$link = mysqli_connect("localhost", 'root', '7939$584&79eD', "cowbtool_main");
-			$sql_ID = mysqli_query($link, "select * from admin_sheet where admin='$dbname'");
+			$sql_ID = mysqli_query($link, "select * from user_list where user_admin='$dbname'");
 			$sql_data = $sql_ID->fetch_assoc();
 			if(empty($sql_data)){
-//			header("location: http://cowbtool.com");
-//			exit;
-				$sql_data['password'] = 'main';
+			header("location: http://cowbtool.com");
+			exit;
+				$sql_data['code'] = 'main';
 
 			}
 			mysqli_close($link);
 		}else{
-			$sql_data['password'] = 'main';
+			$sql_data['code'] = 'main';
 		}
 	}else{
-		$sql_data['password'] = 'main';
+		$sql_data['code'] = 'main';
 	}
 
 	return [
 		'class' => 'yii\db\Connection',
-		'dsn' => 'mysql:host=localhost;dbname=cowbtool_'.$sql_data['password'],
+		'dsn' => 'mysql:host=localhost;dbname=cowbtool_'.$sql_data['code'],
 		'username' => 'root',
 		'password' => '7939$584&79eD',
 		'charset' => 'utf8',
-
-		// Schema cache options (for production environment)
-		//'enableSchemaCache' => true,
-		//'schemaCacheDuration' => 60,
-		//'schemaCache' => 'cache',
 	];
 	return [
 		'class' => 'yii\db\Connection',
@@ -39,8 +33,4 @@
 		'password' => 'andrew623401',
 		'charset' => 'utf8',
 
-		// Schema cache options (for production environment)
-		//'enableSchemaCache' => true,
-		//'schemaCacheDuration' => 60,
-		//'schemaCache' => 'cache',
 	];
