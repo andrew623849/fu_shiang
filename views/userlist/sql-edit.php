a57daaa94165bff008cff0cf7a47931956286eb5
andrew623849 <K122918510@gmail.com>
2020-04-07 20:33:21 +0800

    sql語法產生器

--- views/userlist/sql-edit.php
+++ views/userlist/sql-edit.php
@@ -10,22 +10,17 @@ $data = UserList::find()->asArray()->all();
 $sql_data = [];
 $sql_title = 'cowbtool_';
 foreach($data as $val){
-	$sql_data[] = $sql_title.$val['code'];
+	$sql_data[$sql_title.$val['code']] = $sql_title.$val['code'];
 }
-$sql_data[] = $sql_title.'main';
-$sql_data[] = $sql_title.'sample';
-$form = ActiveForm::begin([
-            'action' => ['/toothcase/pdf'],
-            'method' => 'post',
-			'options'=>['target'=>'_blank']
-        ]);
-	$mult_sls = MultiSelectListBox::widget([
-		'id' => 'new_multi_select1',
-		'name' => 'sql',
-		"options" => ['multiple'=>"multiple"], // for the actual multiselect
-		'data' => $sql_data, // data as array
-		'value' => '', // if preselected
-	]);
+$sql_data[$sql_title.'main'] = $sql_title.'main';
+$sql_data[$sql_title.'sample'] = $sql_title.'sample';
+$mult_sls = MultiSelectListBox::widget([
+	'id' => 'new_multi_select1',
+	'name' => 'sql',
+	"options" => ['multiple'=>"multiple"], // for the actual multiselect
+	'data' => $sql_data, // data as array
+	'value' => '', // if preselected
+]);
 ?>
 	<div class="form-group">
 		<label class="col-md-2 control-label">選擇資料庫</label>
@@ -60,7 +55,7 @@ $form = ActiveForm::begin([
 		<label class="col-md-2 control-label"></label>
 		<div class="col-md-10">
 			<p>
-				<?php echo Html::SubmitButton('語法產生',['class'=> 'btn btn-success']);?>
+				<?php echo Html::Button('語法產生',['class'=> 'btn btn-success','onclick'=>'sqlcreate()']);?>
 			</p>
 		</div>
 	</div>
@@ -68,12 +63,10 @@ $form = ActiveForm::begin([
 	<div class="form-group">
 		<label class="col-md-2 control-label">產生語法</label>
 		<div class="col-md-10">
-			<textarea rows="6" style="width: 100%;resize: vertical;"></textarea>
+			<textarea name="create_sql" rows="6" style="width: 100%;resize: vertical;"></textarea>
 		</div>
 	</div>
 <?php
-ActiveForm::end();
-
 
 $js=<<<JS
 	function new_multi_select_all(){
@@ -81,6 +74,20 @@ $js=<<<JS
 	}
 	function new_multi_deselect_all(){
 		$('#new_multi_select1').multiSelect('deselect_all');
+	}
+	function sqlcreate(){
+		var this_sql_string = $("textarea[name=sql_string]").val();
+		var select_db = $('#new_multi_select1').val();
+		if(this_sql_string.indexOf("#database") != -1 && select_db){
+			var create_sql_string = '';
+			for(var i = 0 ; i < select_db.length ; i++){
+				create_sql_string += this_sql_string.replace(new RegExp("#database","g"),select_db[i]);
+				if((select_db.length-1) != i)
+					create_sql_string += "\\n";
+			}
+			$("textarea[name=create_sql]").val(create_sql_string);
+		}else
+			$("textarea[name=create_sql]").val(this_sql_string);
 	}
 JS;
 $this->registerJs($js, View::POS_END);
