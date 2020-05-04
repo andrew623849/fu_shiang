0a7f17277d6dc9833eb3d5aee271d2afd0964777
andrew623849 <K122918510@gmail.com>
2020-05-05 02:11:27 +0800

    分頁編輯

--- views/site/pages.php
+++ views/site/pages.php
@@ -1,7 +1,12 @@
+
 <?php
 
 	use app\models\UploadForm;
 
 	$model = new UploadForm();
-
-	require $model->home_file.'/pages/'.$op;
+?>
+<div id="main" class="wrapper style1">
+	<section class="container">
+<?php require $model->home_file.'/pages/'.$op; ?>
+	</section>
+</div>
